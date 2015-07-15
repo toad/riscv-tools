1ec371d62f0245e7f51b86b59bc1a7bf9078454f
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-15 15:00:21 +0100

    Use genisofs, hardcode inittab

--- make_root.sh
+++ make_root.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-dd if=/dev/zero of=root.bin bs=1M count=64
-mkfs.ext2 -F root.bin
+rm -Rf mnt
 mkdir mnt
-sudo mount -o loop root.bin mnt
 cd mnt
-sudo mkdir -p bin etc dev lib proc sbin sys tmp usr usr/bin \
+mkdir -p bin etc dev lib proc sbin sys tmp usr usr/bin \
 usr/lib usr/sbin
 cd ..
-sudo cp busybox mnt/bin
-curl -L http://riscv.org/install-guides/linux-inittab > inittab
-sudo cp inittab mnt/etc/inittab
-sudo ln -s ../bin/busybox mnt/sbin/init
-sudo umount mnt
-
-
+cp busybox-1.21.1/busybox mnt/bin
+#curl -L http://riscv.org/install-guides/linux-inittab > inittab
+cat > inittab <<EOF
+::sysinit:/bin/busybox mount -t proc proc /proc
+::sysinit:/bin/busybox mount -t tmpfs tmpfs /tmp
+::sysinit:/bin/busybox mount -o remount,rw /dev/htifbd0 /
+::sysinit:/bin/busybox --install -s
+/dev/ttyHTIF0::sysinit:-/bin/ash 
+EOF
+cp inittab mnt/etc/inittab
+ln -s ../bin/busybox mnt/sbin/init
+genext2fs -d mnt -b 65536 -N 1024 -q root.bin
