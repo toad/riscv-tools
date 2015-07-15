0bf2a67bf90974ad8dbb31b37af59e9c85fab316
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-15 16:13:58 +0100

    Extend make_root.sh for specific binaries

--- make_root.sh
+++ make_root.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+RUN=${1:-/bin/ash}
 rm -Rf mnt
 mkdir mnt
 cd mnt
 mkdir -p bin etc dev lib proc sbin sys tmp usr usr/bin \
 usr/lib usr/sbin
+if ! test "$RUN" == "/bin/ash"
+then
+	NAME=$(basename $RUN)
+	if test ! -f ../$RUN; then echo "./make_root.sh [filename]"; exit; fi
+	cp ../$RUN bin/$NAME
+	RUN="/bin/$NAME"
+fi
 cd ..
 cp busybox-1.21.1/busybox mnt/bin
 #curl -L http://riscv.org/install-guides/linux-inittab > inittab
@@ -12,8 +20,8 @@ cat > inittab <<EOF
 ::sysinit:/bin/busybox mount -t tmpfs tmpfs /tmp
 ::sysinit:/bin/busybox mount -o remount,rw /dev/htifbd0 /
 ::sysinit:/bin/busybox --install -s
-/dev/ttyHTIF0::sysinit:-/bin/ash 
 EOF
+echo /dev/ttyHTIF0::sysinit:-$RUN >> inittab
 cp inittab mnt/etc/inittab
 ln -s ../bin/busybox mnt/sbin/init
 genext2fs -d mnt -b 65536 -N 1024 -q root.bin
